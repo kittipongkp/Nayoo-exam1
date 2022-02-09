f1990d9438b0f875c81bcf3a53c76ff4cde7cd0d
kittipongkp <kittipongga@gmail.com>
2022-02-10 01:05:42 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div style={{backgroundColor: "#101010", height: 1200 , width: "80%", margin: 'auto'}} >
+    
+    </div>
+  )
+}
+export default Home
