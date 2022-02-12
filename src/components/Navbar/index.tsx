f3a557a45dd5ce0d13d4e13855a76f3d7fee5bbe
kittipongkp <kittipongga@gmail.com>
2022-02-12 10:08:14 +0700

    เพิ่ม Ads banner Carouse

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,13 +6,14 @@ const NavBar: React.FC<Props> = () => {
 
 
     return (
-        <div className="fixed-top">
+        <div style={{ position: "fixed", width: '100%', marginRight: 'auto', zIndex: 1000}}>
         <div style={{ width: '100%', height: 85, backgroundColor: "#FFFFFF", border: '1px solid #C4C4C4', display: 'flex', flexDirection: "column" }} >
             <div style={{ height: 40, width: '80%', backgroundColor: "#FFFFFF", margin: 'auto', flexDirection: "row", flex: 1, display: 'flex' }} >
                 <div style={{ flex: 0.5  }} >
                     <h2  style={{ marginTop: 5}} >LOGO</h2>
                 </div  >
-                <div className="input-group mb-3" style={{flex: 3 , marginTop: 7, marginLeft: 5 }}>
+                <div className="input-group mb-3  inner-addon left-addon" style={{flex: 3 , marginTop: 7, marginLeft: 5 }}>
+                <i className="fa-solid fa-magnifying-glass"></i>
                     <input type="text" className="form-control" placeholder="ค้นหน้า" aria-label="Username" aria-describedby="basic-addon1" style={{backgroundColor: "#E5E5E5", padding: '0.27rem' ,borderRadius: "0.3rem", border: 'none',}} />
                 </div>
                 <div style={{ flex: 6  }} >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import Carosel from "components/Carousel";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div style={{backgroundColor: "#101010", height: 1200 , width: "80%", margin: 'auto'}} >
-    
+    <div
+      style={{ height: 1500, width: "100%", display: "flex" }}
+    >
+      <div style={{ width: '80%', margin: "auto", display: "flex", marginTop: 0 }}>
+        <div style={{ marginTop: 100, width: "100%" }}>
+          <Carosel />
+        </div>
+      </div>
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
