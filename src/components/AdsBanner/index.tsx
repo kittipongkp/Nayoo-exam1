bd6825a078e580556b2aaf0012507d2ac2392f10
kittipongkp <kittipongga@gmail.com>
2022-02-18 04:57:49 +0700

    Update index.tsx

--- src/components/AdsBanner/index.tsx
+++ src/components/AdsBanner/index.tsx
@@ -5,15 +5,15 @@ const AdsBanner = () => {
   const [show, setShow] = useState(true);
 
   return (
-    <div className="container-xxl" style={{ position: "fixed", marginTop: 80, zIndex: 100, display : show == true ? "block" : "none", }} >
+    <div className="container-xxl  position-fixed" style={{  marginTop: 100, zIndex: 100, display : show == true ? "block" : "none", }} >
     <div className="row" style={{ marginTop: 0 }} >
-      <div className='col-3' style={{ backgroundColor: "#C4C4C4", height: 590 }}> <div style={{ marginLeft: 5 }}>
+      <div className='col-3' style={{ backgroundColor: "#C4C4C4", height: 590 , width: 150}}> <div style={{ marginLeft: 5 }}>
          <button type="button" className="close" aria-label="Close" onClick={()=> setShow(false) }  >
             <span aria-hidden="true">X</span>
           </button>
          </div> </div>
       <div className='col-6' style={{  }} ></div>
-      <div className='col-3' style={{ backgroundColor: "#C4C4C4", height: 590 }}  >
+      <div className='col-3' style={{ backgroundColor: "#C4C4C4", height: 590, width: 150 }}  >
       <div style={{ marginLeft: 5 }}>
          <button type="button" className="close" aria-label="Close" onClick={()=> setShow(false) }  >
             <span aria-hidden="true">X</span>
